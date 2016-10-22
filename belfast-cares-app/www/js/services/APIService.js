b6b52773c83309b027c52c7b498a7942db3066ce
Apoclyps <kyle90adam@hotmail.com>
2016-10-22 11:36:21 +0100

    Adding some content to the homepage

--- belfast-cares-app/www/js/services/APIService.js
+++ belfast-cares-app/www/js/services/APIService.js
@@ -3,14 +3,14 @@
  */
 angular.module('starter.apiservice', ['ionic', 'ionic-material'])
 
-  .factory('Organisations', function($http) {
+  .factory('Organisations', function ($http) {
     var dataSource = 'http://demo3929519.mockable.io/organisation';
 
     return {
-      getOrganisations: function() {
+      getOrganisations: function () {
         return $http.get(dataSource);
       },
-      getOrganisation: function(comicId) {
+      getOrganisation: function (comicId) {
         return $http.get(dataSource, {
           params: {
             id: comicId
@@ -20,15 +20,38 @@ angular.module('starter.apiservice', ['ionic', 'ionic-material'])
     }
   })
 
+  .factory('Auth', function ($scope, $http) {
+    var auth = 'http://belfastcares.co.uk/api-token-auth/';
 
-  .factory('Comics', function($http) {
+    return {
+      getToken: function () {
+
+        var data = {
+          json: JSON.stringify({
+              username: 'admin',
+              password: 'bcadminpass123'
+            }
+          )
+        };
+
+        return $http.post(auth, data).success(function (data, status) {
+          $scope.hello = data;
+          console.log(data);
+        });
+
+      }
+    }
+  })
+
+
+  .factory('Comics', function ($http) {
     var dataSource = 'http://samcroft.co.uk/comics-app/comics?callback=JSON_CALLBACK';
 
     return {
-      getComics: function() {
+      getComics: function () {
         return $http.jsonp(dataSource);
       },
-      getComic: function(comicId) {
+      getComic: function (comicId) {
         return $http.jsonp(dataSource, {
           params: {
             id: comicId
